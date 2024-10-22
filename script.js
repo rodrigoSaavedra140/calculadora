c5d7767f2a6f6ff8d05fe06e7bb836d33b20a71e
Saavedra Diaz Hugo Rodrigo <hugorodrigo.saavedradiaz@gmail.com>
2024-10-22 14:17:46 -0300

    add comments and clear code

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const screen = document.querySelector(".screen");
 const hist = document.querySelector(".historial");
 var parenthesis = '';
 
+//funcion que agrega al final del input el caracter selecionado
 function appendToScreen(input){
+    //verifica si el string ya tiene un operador o signo que no sea un numero y los borra
     if(isNaN(screen.value[screen.value.length -1]) && (isNaN(input)) && (input !== '-')){
         if(screen.value[screen.value.length -1] === '('){
             delScreen();
@@ -10,6 +12,7 @@ function appendToScreen(input){
         }
         delScreen();
     }
+    //condicion para agregar los parentesis o otro signo, dependiendo si ya se abrio o cerro anteriormente
     if(input === 'p'){
         if (parenthesis === '(' ){
             screen.value += ')';
@@ -26,24 +29,27 @@ function appendToScreen(input){
     }
     
 }
-
+// funcion para procesar el string y devolver por pantalla el resultado del mismo
 function equal(){
-    
+    //corrobora si el string termina en un operador o signo, sin contar al cierre de parentesis
     if(isNaN(screen.value[screen.value.length -1]) && (screen.value[screen.value.length -1] !== ')')){
         delScreen();
     }
+    // cierra el parentesis, si no esta cerrado
     if (parenthesis === '('){
         screen.value += ')';
         parenthesis = ''; 
     }
+    // muestra el resultado por pantalla y toda la operacion hecha anteriormente
     hist.innerText = screen.value;
     screen.value = eval(screen.value);
 }
-
+// limpia todo el input
 function clearScreen() {
     screen.value = "";
     hist.innerText = "";
 }
+// limpia solo el ultimo caracter del input
 function delScreen (){
     screen.value = screen.value.slice(0,-1);
 }
